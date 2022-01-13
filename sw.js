13defa51d88d73690ff2e3b4893dd9d7f4ca019f
JacobLinCool <jacoblincool@users.noreply.github.com>
2022-01-13 16:32:23 +0000

    deploy: 705be9c056bcecc33d6f8bd73e7f0a1b5949e4d6

--- sw.js
+++ sw.js
@@ -96,35 +96,35 @@ define(['./workbox-e172ad1d'], (function (workbox) { 'use strict';
     "url": "assets/api.a6e8cdff.js",
     "revision": "afe9597fc7fa66ebf9114292eeb60003"
   }, {
-    "url": "assets/Create.6e9da2dc.js",
-    "revision": "8f2a0f69ba98721bcf7e0fea30916cac"
+    "url": "assets/Create.0bb47368.js",
+    "revision": "5ce7aae0533e0923a9008fc7de61d06d"
   }, {
     "url": "assets/Create.7feb3a1c.css",
     "revision": "dd7bece12bbdcfd833dcf3359c8bef46"
   }, {
-    "url": "assets/Home.3eb4ca8d.js",
-    "revision": "62b5ea25e6217a9ff53a0e6a4f1c6e20"
+    "url": "assets/Home.2330c095.js",
+    "revision": "b20dad55864242e7bac1c26abd6e5516"
   }, {
-    "url": "assets/Home.c13e0ff5.css",
-    "revision": "e367aa5e8df730ba03c8311c1d16ddbd"
+    "url": "assets/Home.acbe76fe.css",
+    "revision": "c4e577260f3e5aa14b63deb3ced04f08"
   }, {
     "url": "assets/index.3300764c.css",
     "revision": "69bd5b6fa4b65e0507cf796c888c4331"
   }, {
-    "url": "assets/index.77b72fb4.js",
-    "revision": "a293419a0a7e20927b5e336a8eae9b70"
+    "url": "assets/index.daebbcbd.js",
+    "revision": "090dd9d093bf0ed29c0511513d3b1e1e"
+  }, {
+    "url": "assets/Play.180136bf.js",
+    "revision": "15dd12d3dced1e5ca5c2d7e3bc4ffadd"
   }, {
     "url": "assets/Play.2c51cbfd.css",
     "revision": "c659af7078e74925de225b8e2432781f"
   }, {
-    "url": "assets/Play.f90e5118.js",
-    "revision": "5ad0616049d9aea7b5dab38f606e5597"
+    "url": "assets/Ranking.4a2aced8.css",
+    "revision": "c5c3ac82116cfec743afb8f5943dba84"
   }, {
-    "url": "assets/Ranking.608ace10.css",
-    "revision": "dbd68d835f22ae3d9a7bae4fb61c8e58"
-  }, {
-    "url": "assets/Ranking.a1e5f110.js",
-    "revision": "8f9f5be11ebc9079cd3701e684545f1a"
+    "url": "assets/Ranking.80e0a12f.js",
+    "revision": "ce44d6fb86acb5f9efcfc89353fc6086"
   }, {
     "url": "assets/sweetalert2.314ae3be.js",
     "revision": "0b9eaca7c8de09dfa597d421f542c146"
@@ -133,7 +133,7 @@ define(['./workbox-e172ad1d'], (function (workbox) { 'use strict';
     "revision": "e7b93d3ed382ead5be1a874beec3671c"
   }, {
     "url": "index.html",
-    "revision": "de5e79cc3f1fa16dcaebed8ad09b3804"
+    "revision": "8abe42758e222c6a65b3f865aa04fcad"
   }, {
     "url": "registerSW.js",
     "revision": "1872c500de691dce40960bb85481de07"
